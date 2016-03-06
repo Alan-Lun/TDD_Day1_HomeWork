9df51eae812c1367672e966426a9d8b29649d165
黃振倫 <s52076663135@gmail.com>
2016-03-06 14:57:45 +0800

    初始建立TestCase

---
+++ ApplicationServiceTests/Service/Item/AS_ItemPriceTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ApplicationService.Service.Item;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationService.Service.Item.Tests
+{
+    [TestClass()]
+    public class AS_ItemPriceTests
+    {
+        [TestMethod()]
+        public void GetSumByCostListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetSumByRevenueListTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
